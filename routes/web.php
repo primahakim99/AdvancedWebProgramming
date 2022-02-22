bd93278c217a15852688fc06bb12ea841907e824
primahakim99 <fahreza.prima@gmail.com>
2022-02-22 14:29:12 +0700

    JB_2_Practicum1

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,17 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//Practicum 1
 Route::get('/', function () {
-    return view('welcome');
+    echo "Hi! Welcome To Laravel"; 
+});
+
+Route::get('/about', function () {
+    echo "Fahreza Prima Hakim<br>"; 
+    echo "2041720242<br>"; 
+    echo "TI - 2I"; 
+});
+
+Route::get('/about/{id}', function ($id) {
+    echo "This is Article Pages with ID: " .$id; 
 });
